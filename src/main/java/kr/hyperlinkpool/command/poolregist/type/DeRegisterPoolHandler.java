0eee74c5fede0774f6e3df6db38c1e334b6e24af
hyperlinkpool <hyperlinkpool@gmail.com>
2020-10-21 23:01:37 +0900

    added notification

--- src/main/java/kr/hyperlinkpool/command/poolregist/type/DeRegisterPoolHandler.java
+++ src/main/java/kr/hyperlinkpool/command/poolregist/type/DeRegisterPoolHandler.java
@@ -4,12 +4,13 @@ import kr.hyperlinkpool.command.AbstractCommandHandler;
 import kr.hyperlinkpool.domain.ProcessResponse;
 import kr.hyperlinkpool.domain.ProcessResultDomain;
 import kr.hyperlinkpool.i18n.MessageFactory;
+import kr.hyperlinkpool.utils.MessagePrompter;
 
 public class DeRegisterPoolHandler extends AbstractCommandHandler{
 
 	@Override
 	public ProcessResultDomain<ProcessResponse> handleCommand(Object... commands) {
-		MessageFactory.getInstance().getMessage("개발중입니다.", "E99999");
+		MessagePrompter.promptMessage(MessageFactory.getInstance().getMessage("개발중입니다.", "E99999"), true);
 		return null;
 	}
 
